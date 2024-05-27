af3b22e4aa522456822cdf95cbf3d8d9b2ae5e83
Zhafa Anbiya <zhafaanbiya311@gmail.com>
2024-05-27 13:31:23 +0700

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE poi_db;
+
+USE poi_db;
+
+CREATE TABLE poi (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    latitude DECIMAL(9, 6) NOT NULL,
+    longitude DECIMAL(9, 6) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    address VARCHAR(255),
+    category VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
